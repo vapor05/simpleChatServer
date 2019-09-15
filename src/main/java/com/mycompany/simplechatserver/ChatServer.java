bf51f0864b7ec621308ccf2afb71ebd0f7822ad1
NBOCCHINI <nicholasbocchini@gmail.com>
2019-09-15 16:41:45 -0500

    Initial commit of ChatHandler class

--- src/main/java/com/mycompany/simplechatserver/ChatServer.java
+++ src/main/java/com/mycompany/simplechatserver/ChatServer.java
@@ -5,10 +5,28 @@
  */
 package com.mycompany.simplechatserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  *
  * @author NicholasBocchini
  */
 public class ChatServer {
+    public ChatServer(int port) throws IOException 
+    {
+        ServerSocket s = new ServerSocket(port);
+        while (true)
+        {
+            new ChatHandler(s.accept()).start();
+        }
+    }
     
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length != 1)
+            throw new RuntimeException(
+                "Syntax: java ChatServer <port>");
+        new ChatServer(Integer.parseInt(args[0]));
+    }
 }
