8ff704d9060cfce6717bfe5faa8d704eb9502b96
NBOCCHINI <nicholasbocchini@gmail.com>
2019-09-15 16:43:00 -0500

    Initial commit of ChatHandler class

--- src/main/java/com/mycompany/simplechatserver/ChatHandler.java
+++ src/main/java/com/mycompany/simplechatserver/ChatHandler.java
@@ -5,10 +5,28 @@
  */
 package com.mycompany.simplechatserver;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author NicholasBocchini
  */
 public class ChatHandler {
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+    static Set<ChatHandler> handlers = new HashSet<ChatHandler>();
     
+    public ChatHandler(Socket socket) throws IOException
+    {
+        this.socket = socket;
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        handlers.add(this);
+    }
 }
